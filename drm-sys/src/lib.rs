724b4e467528912772e1a729006d9ddcf1c93463
K <kayo@illumium.org>
2021-03-17 18:55:00 +0500

    Updated pre-generated bindings

--- drm-sys/src/lib.rs
+++ drm-sys/src/lib.rs
@@ -14,6 +14,16 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 include!(concat!("platforms/linux/x86_64/bindings.rs"));
 
 #[cfg(all(not(feature = "use_bindgen"),
+          target_os="linux",
+          target_arch="i686"))]
+include!(concat!("platforms/linux/i686/bindings.rs"));
+
+#[cfg(all(not(feature = "use_bindgen"),
+          target_os="linux",
+          target_arch="arm"))]
+include!(concat!("platforms/linux/arm/bindings.rs"));
+
+#[cfg(all(not(feature = "use_bindgen"),
           target_os="freebsd",
           target_arch="x86_64"))]
 include!(concat!("platforms/freebsd/x86_64/bindings.rs"));
