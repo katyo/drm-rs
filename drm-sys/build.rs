44a3646fe904875ce7f7d83ccfa8f937ac55bd85
K <kayo@illumium.org>
2021-03-17 18:21:52 +0500

    Fixed use_bindgen feature

--- drm-sys/build.rs
+++ drm-sys/build.rs
@@ -3,76 +3,134 @@ mod use_bindgen {
     extern crate bindgen;
     extern crate pkg_config;
 
-    use self::bindgen::Builder;
+    use self::bindgen::{Builder, CodegenConfig};
     use std::env::var;
     use std::path::PathBuf;
-    use std::fs::File;
-    use std::io::Write;
 
-    const MACROS: &'static [&str] = &[
-        "DRM_MODE_PROP_SIGNED_RANGE",
-        "DRM_MODE_PROP_OBJECT"
-    ];
-
-    // Unfortunately the cexpr crate (and as such, bindgen) does not support C
-    // functional macros (https://github.com/jethrogb/rust-cexpr/issues/3).
-    // Therefore we must create them ourselves.
-    fn bind_function_macro(name: &str) -> String {
-        let temp_bind = "const unsigned int _".to_string() + name + " = " + name + ";\n";
-        let undef = "#undef ".to_string() + name + "\n";
-        let new_bind = "const unsigned int ".to_string() + name + " = _" + name + ";\n";
-
-        temp_bind + &undef + &new_bind
-    }
-
-    pub fn generate_header() {
-        let out_path = String::from(var("OUT_DIR").unwrap());
-        let header = out_path.clone() + "/bindings.h";
-
-        let mut f = File::create(header).expect("Could not create header");
-        let includes = "#include <drm.h>\n#include <drm_mode.h>\n".to_string();
-        f.write(includes.as_bytes()).expect("Could not write header.");
-
-        for m in MACROS {
-            f.write(bind_function_macro(m).as_bytes())
-                .expect("Could not write header");
-        }
-    }
-
-    pub fn generate_bindings() {
-        let out_path = String::from(var("OUT_DIR").unwrap());
-        let header = out_path.clone() + "/bindings.h";
+    fn create_builder(contents: &str) -> Builder {
+        println!("{}", contents);
 
         let pkgconf = pkg_config::Config::new();
         let lib = pkgconf.probe("libdrm").unwrap();
 
-        let mut builder = Builder::default()
-            .header(header)
+        let config = CodegenConfig::all();
+
+        Builder::default()
+            .clang_args(
+                lib.include_paths
+                    .into_iter()
+                    .map(|path| "-I".to_string() + &path.into_os_string().into_string().unwrap()),
+            )
+            .header_contents("bindings.h", contents)
             .ctypes_prefix("libc")
-            .emit_builtins()
-            .emit_clang_ast()
-            .emit_ir()
+            .with_codegen_config(config)
+            .prepend_enum_name(false)
+            .layout_tests(false)
+            .generate_comments(false)
+            .rustfmt_bindings(true)
+            .derive_copy(true)
             .derive_debug(true)
-            .derive_default(true);
+            .derive_default(true)
+            .derive_hash(true)
+            .derive_eq(true)
+            .whitelist_recursively(true)
+            .use_core()
+    }
 
-        for path in lib.include_paths {
-            let arg = "-I".to_string() + &path.into_os_string().into_string().unwrap();
-            builder = builder.clang_arg(arg)
-        }
-        let bindings = builder.generate().expect("Unable to generate libdrm bindings");
+    const TMP_BIND_PREFIX: &str = "__BINDGEN_TMP_";
+    const TMP_BIND_PREFIX_REG: &str = "_BINDGEN_TMP_.*";
 
+    const INCLUDES: &'static [&str] = &["drm.h", "drm_mode.h"];
+
+    const MACROS: &'static [&str] = &["DRM_MODE_PROP_SIGNED_RANGE", "DRM_MODE_PROP_OBJECT"];
+
+    // Applies a formatting function over a slice of strings,
+    // concatenating them on separate lines into a single String
+    fn apply_formatting<I, F>(iter: I, f: F) -> String
+    where
+        I: Iterator,
+        I::Item: AsRef<str>,
+        F: Fn(&str) -> String,
+    {
+        iter.fold(String::new(), |acc, x| acc + &f(x.as_ref()) + "\n")
+    }
+
+    // Create a name for a temporary value
+    fn tmp_val(name: &str) -> String {
+        format!("{}{}", TMP_BIND_PREFIX, name)
+    }
+
+    // Create a C include directive
+    fn include(header: &str) -> String {
+        format!("#include <{}>", header)
+    }
+
+    // Create a C constant
+    fn decl_const(ty: &str, name: &str, value: &str) -> String {
+        format!("const {} {} = {};", ty, name, value)
+    }
+
+    // Create a C macro definition
+    fn define_macro(name: &str, val: &str) -> String {
+        format!("#define {} {}", name, val)
+    }
+
+    // Create a C undefinition
+    fn undefine_macro(name: &str) -> String {
+        format!("#undef {}", name)
+    }
+
+    // Rebind a C macro as a constant
+    // Required for some macros that won't get generated
+    fn rebind_macro(name: &str) -> String {
+        let tmp_name = tmp_val(name);
+        format!(
+            "{}\n{}\n{}\n{}",
+            decl_const("unsigned int", &tmp_name, name),
+            undefine_macro(name),
+            decl_const("unsigned int", name, &tmp_name),
+            define_macro(name, name)
+        )
+    }
+
+    // Fully create the header
+    fn create_header() -> String {
+        apply_formatting(INCLUDES.iter(), include) + &apply_formatting(MACROS.iter(), rebind_macro)
+    }
+
+    pub fn generate_bindings() {
+        let bindings = create_builder(&create_header())
+            .blacklist_type(TMP_BIND_PREFIX_REG)
+            .blacklist_type("drm_control_DRM_ADD_COMMAND")
+            .whitelist_type("DRM_.*|drm_.*")
+            .whitelist_var("DRM_.*|drm_.*")
+            .constified_enum_module("drm_control_.*")
+            .constified_enum_module("drm_buf_desc_.*")
+            .constified_enum_module("drm_map_type")
+            .constified_enum_module("drm_map_flags")
+            .constified_enum_module("drm_stat_type")
+            .constified_enum_module("drm_lock_flags")
+            .constified_enum_module("drm_dma_flags")
+            .constified_enum_module("drm_ctx_flags")
+            .constified_enum_module("drm_drawable_info_type_t")
+            .constified_enum_module("drm_vblank_seq_type")
+            .constified_enum_module("drm_mode_subconnector")
+            .generate()
+            .expect("Unable to generate libdrm bindings");
+
+        let out_path = String::from(var("OUT_DIR").unwrap());
         let bind_file = PathBuf::from(out_path).join("bindings.rs");
 
-        bindings.write_to_file(bind_file).expect("Could not write bindings");
+        bindings
+            .write_to_file(bind_file)
+            .expect("Could not write bindings");
     }
 }
 
 #[cfg(feature = "use_bindgen")]
 pub fn main() {
-    use_bindgen::generate_header();
     use_bindgen::generate_bindings();
 }
 
 #[cfg(not(feature = "use_bindgen"))]
 pub fn main() {}
-
